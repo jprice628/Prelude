031bdf810e655f65462a10ce4dde39a405353599
jprice628 <jprice628@hotmail.com>
2025-01-11 17:39:25 -0500

    Added git config.

--- Git/Configure-Aliases.ps1
+++ Git/Configure-Aliases.ps1
@@ -1,12 +1,5 @@
-# Aliases
 git config --global alias.unstage 'restore --staged'
 git config --global alias.cleanup 'clean -d -f'
 git config --global alias.last5   'log --oneline -5'
 git config --global alias.last10  'log --oneline -10'
 git config --global alias.last20  'log --oneline -20'
-
-# Auto Setup Remote
-git config --global push.autoSetupRemote true
-
-# Use Neovim
-git config --global core.editor nvim
